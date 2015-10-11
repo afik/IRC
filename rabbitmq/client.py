a227df97b70da969144325c4c004902025ddb7b4
Junita Sinambela <jujunale@gmail.com>
2015-10-11 21:29:25 +0700

    TODO 123 done

--- rabbitmq/client.py
+++ rabbitmq/client.py
@@ -1,33 +1,29 @@
 #TODO : 
-# 1- gr2 tiap /NICK bind ke exchange semua user nerima semua pesan yg dikirim pake sendAll
-# 2- exit untuk multithread
-# 3- unbinding pas leave
 # 4- pretty format buat print received messages
 
 import pika, threading, time
 
 class ReceiveThread(threading.Thread):
 	lock = threading.Lock()
-	def __init__(self, queuename):
+	def __init__(self, queuename, host):
 		threading.Thread.__init__(self)
 		ReceiveThread.lock.acquire()
 		self.queuename = queuename
+		self.host = host
 		ReceiveThread.lock.release()
 	def run(self):
-		print("Queue : ", self.queuename)
 		connectiontemp = pika.BlockingConnection(pika.ConnectionParameters(
-		        host='localhost')) #167.205.32.46
+		        host=self.host)) #167.205.32.46
 		channeltemp = connectiontemp.channel()
 		channeltemp.queue_declare(queue=self.queuename, durable=True)
 		channel.basic_qos(prefetch_count=1)
 
 		def callback(ch, method, properties, body):
-		    print (body) #TODO 4
+		    print ("%r" % (body, ))
 		    ch.basic_ack(delivery_tag = method.delivery_tag) 
 
 		channeltemp.basic_consume(callback, queue=self.queuename)
 		channeltemp.start_consuming()
-
 
 
 def splitstring(command):
@@ -36,29 +32,27 @@ def splitstring(command):
 
 def setNickName(nickName):
 	global nickname
+	nicktemp = nickname
 	nickname = nickName
-	global queuename
-	queuename = '13512023_' + nickname
-	result = channel.queue_declare(queue=queuename, durable=True)
-	channel.queue_bind(exchange=exchange, queue=queuename) #TODO 1
-	thread = ReceiveThread(queuename)
-	thread.start()
-	global listchannelthread
-	listchannelthread += [thread]
+	if(nicktemp == ""):									# check if nick empty at first, if true, create receivethread
+		global queuename
+		queuename = nickname
+		result = channel.queue_declare(queue=queuename, durable=True)
+		global receivethread
+		receivethread = ReceiveThread(queuename, host)
+		receivethread.start()
 	print("Your nickname is ", nickname)
 
 def joinChannel(channelname):
-	channelname = '13512023_' + channelname
 	channel.queue_bind(exchange=exchange, queue=queuename, routing_key=channelname)
 	global listchannel
 	listchannel += [channelname]
-	print (listchannel)
-	print("You join  channel")
+	print("You join ", channelname, " channel")
 
 def leaveChannel(channelname):
-	channelname = '13512023_' + channelname
 	def removechannel(channelname):
-		listchannel.remove(channelname) #TODO 3
+		channel.queue_unbind(queue=queuename, exchange=exchange, routing_key=channelname)
+		listchannel.remove(channelname)
 		print("You leave ", channelname, " channel")
 	if(channelname in listchannel):
 		removechannel(channelname)
@@ -66,16 +60,29 @@ def leaveChannel(channelname):
 		print("No ", channelname, " channel")
 
 def sendMessage(message):
-	message = " (" + nickname + ") " + message
-	for channelname in listchannel:
-		#message_body = "Message from " + nickname + " in " + channelname + " : " + message
-		channel.basic_publish(exchange=exchange, routing_key=channelname, body=message) #TODO 4
-		# message_body = ""
+	if(len(listchannel) > 0):
+		for channelname in listchannel:
+			message = "[" + channelname + "] (" + nickname + ") " + message
+			channel.basic_publish(exchange=exchange, routing_key=channelname, body=message)
+			message = ""
+	else:
+		print("No channel")
 
 def sendMessageTo(channelname, message):
-	#message = "Message from " + nickname + " in " + channelname + " : " + message
-	message = " (" + nickname + ") " + message
-	channel.basic_publish(exchange=exchange, routing_key=channelname, body=message) #TODO 4
+	message = "[" + channelname + "] (" + nickname + ") " + message
+	if(len(listchannel) > 0):
+		if(channelname in listchannel):
+			channel.basic_publish(exchange=exchange, routing_key=channelname, body=message) #TODO 4
+		else:
+			print("No ", channelname, " channel")
+	else:
+		print("No channel")
+
+def exit():
+	print("Good bye")
+	channel.queue_delete(queue=queuename)
+	connection.close()
+	raise SystemExit #TODO 2
 
 def callMethod(command):		
 	commandlist = splitstring(command)
@@ -88,16 +95,16 @@ def callMethod(command):
 			elif(commandlist[0] == "/LEAVE"):
 				leaveChannel(commandlist[1])
 			elif(commandlist[0] == "/EXIT"):
-				connection.close()
-				print("Good bye")
-				channel.queue_delete(queue=queuename)
-				raise SystemExit #TODO 2
-			else:
+				exit()
+			else :
 				if(commandlist[0].startswith('@')):
-					splitting = commandlist[0].split("@", maxsplit=1)
-					sendMessageTo(splitting[1], commandlist[1])
+					if(len(commandlist) > 1):
+						splitting = commandlist[0].split("@", maxsplit=1)
+						sendMessageTo(splitting[1], commandlist[1])
 				else:
 					sendMessage(command)
+		else:
+			print("Set your nickname first")
 
 def sendthread():
 	commandtemp = input()
@@ -109,19 +116,18 @@ def main():
 	        host=host))
 	global channel
 	channel = connection.channel()
-	channel.exchange_declare(exchange="13512023", type="fanout")
+	channel.exchange_declare(exchange=exchange, exchange_type="topic", auto_delete=True)
 
 	while(True):
 		sendthread()
 
-
 nickname = ""
 queuename = ""
 listchannel = []
-listchannelthread = []
 host = 'localhost' #167.205.32.46
 connection = None
 channel = None
-exchange = "13512023"
+receivethread = None
+exchange = "13512023__"
 
 main()
