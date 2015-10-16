f5b3168292cd1bca1b028a1ad4361451f1aab0dc
afik <k.afis3@rocketmail.com>
2015-10-16 12:14:27 +0700

    try consume

--- kafka-irc/src/if4031/Client.java
+++ kafka-irc/src/if4031/Client.java
@@ -3,6 +3,7 @@ package if4031;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.Properties;
+import java.util.Scanner;
 
 import kafka.producer.KeyedMessage;
 import kafka.producer.ProducerConfig;
@@ -13,7 +14,7 @@ import kafka.producer.ProducerConfig;
  */
 public class Client {
     final static String TOPIC = "javatest";
-
+    
     public static void main(String[] argv){
         Properties properties = new Properties();
         properties.put("metadata.broker.list","localhost:9092");
@@ -21,7 +22,9 @@ public class Client {
         ProducerConfig producerConfig = new ProducerConfig(properties);
         kafka.javaapi.producer.Producer<String,String> producer = new kafka.javaapi.producer.Producer<String, String>(producerConfig);
         SimpleDateFormat sdf = new SimpleDateFormat();
-        KeyedMessage<String, String> message =new KeyedMessage<String, String>(TOPIC,"Test message from java program " + sdf.format(new Date()));
+        Scanner in = new Scanner(System.in);
+        String msg = in.nextLine();
+        KeyedMessage<String, String> message = new KeyedMessage<String, String>(TOPIC,msg + sdf.format(new Date()));
         producer.send(message);
         producer.close();
     }
---
+++ kafka-irc/src/if4031/Consumer.java
@@ -0,0 +1,74 @@
+package if4031;
+
+import kafka.consumer.ConsumerConfig;
+import kafka.consumer.KafkaStream;
+import kafka.javaapi.consumer.ConsumerConnector;
+ 
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Properties;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.TimeUnit;
+import kafka.consumer.ConsumerIterator;
+
+/**
+ *
+ * @author TOLEP
+ */
+public class Consumer {
+    private final ConsumerConnector consumer;
+    private final String topic;
+    private  ExecutorService executor;
+ 
+    public Consumer(String a_zookeeper, String a_groupId, String a_topic) {
+        consumer = kafka.consumer.Consumer.createJavaConsumerConnector(
+                createConsumerConfig(a_zookeeper, a_groupId));
+        this.topic = a_topic;
+    }
+ 
+    public void shutdown() {
+        if (consumer != null) consumer.shutdown();
+        if (executor != null) executor.shutdown();
+        try {
+            if (!executor.awaitTermination(5000, TimeUnit.MILLISECONDS)) {
+                System.out.println("Timed out waiting for consumer threads to shut down, exiting uncleanly");
+            }
+        } catch (InterruptedException e) {
+            System.out.println("Interrupted during shutdown, exiting uncleanly");
+        }
+   }
+ 
+    public void run(int a_numThreads) {
+        Map<String, Integer> topicCountMap = new HashMap<String, Integer>();
+        topicCountMap.put(topic, new Integer(a_numThreads));
+        Map<String, List<KafkaStream<byte[], byte[]>>> consumerMap = consumer.createMessageStreams(topicCountMap);
+        List<KafkaStream<byte[], byte[]>> streams = consumerMap.get(topic);
+        
+        ConsumerIterator<byte[], byte[]> it = streams.get(0).iterator();
+        while (it.hasNext())
+            System.out.println("Message received : " + new String(it.next().message()));
+        System.out.println("Shutting down ");
+    }
+ 
+    private static ConsumerConfig createConsumerConfig(String a_zookeeper, String a_groupId) {
+        Properties props = new Properties();
+        props.put("zookeeper.connect", a_zookeeper);
+        props.put("group.id", a_groupId);
+        props.put("zookeeper.session.timeout.ms", "400");
+        props.put("auto.commit.interval.ms", "1000");
+ 
+        return new ConsumerConfig(props);
+    }
+ 
+    public static void main(String[] args) {
+        String zooKeeper = "localhost:2181";
+        String groupId = "1";
+        String topic = "page_visits";
+ 
+        Consumer example = new Consumer(zooKeeper, groupId, topic);
+        example.run(1);
+
+        example.shutdown();
+    }
+}
